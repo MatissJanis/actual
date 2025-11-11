993d144fe04cf6f1f14401178772700c96a40e38
Matiss Janis Aboltins <matiss@mja.lv>
2025-11-11 19:50:17 +0000

    Update globals.ts

--- packages/desktop-client/globals.ts
+++ packages/desktop-client/globals.ts
@@ -6,6 +6,8 @@ declare module '*.png';
 declare global {
   function __resetWorld(): void;
 
+
+  
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace PlaywrightTest {
     // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
